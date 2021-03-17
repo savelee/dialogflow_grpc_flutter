42a980935a404aa8c3f0476841e65666f8c598b2
Lee Boonstra <lee@leeboonstra.com>
2021-03-17 15:59:14 +0100

    v2beta1 support

--- lib/dialogflow_v2beta1.dart
+++ lib/dialogflow_v2beta1.dart
@@ -4,10 +4,11 @@
 // directory. You can also find a detailed instruction on how to add platforms in the `pubspec.yaml` at https://flutter.dev/docs/development/packages-and-plugins/developing-packages#plugin-platforms.
 
 import 'dart:async';
-import 'package:dialogflow_grpc/generated/google/cloud/dialogflow/v2/audio_config.pb.dart';
-import 'package:dialogflow_grpc/generated/google/cloud/dialogflow/v2/session.pb.dart';
-import 'package:dialogflow_grpc/generated/google/cloud/dialogflow/v2/session.pbgrpc.dart';
-import 'package:dialogflow_grpc/types/v2/input_config.dart';
+import 'package:dialogflow_grpc/dialogflow_grpc.dart';
+import 'package:dialogflow_grpc/generated/google/cloud/dialogflow/v2beta1/audio_config.pb.dart';
+import 'package:dialogflow_grpc/generated/google/cloud/dialogflow/v2beta1/session.pb.dart';
+import 'package:dialogflow_grpc/generated/google/cloud/dialogflow/v2beta1/session.pbgrpc.dart';
+import 'package:dialogflow_grpc/types/v2beta1/input_config.dart';
 import 'package:dialogflow_grpc/dialogflow_auth.dart';
 import 'package:grpc/grpc.dart';
 import 'package:uuid/uuid.dart';
@@ -16,7 +17,7 @@ import 'package:uuid/uuid.dart';
 /// An interface to Google Cloud's Dialogflow API via gRPC
 ///
 /// You will need a Dialogflow project and a service account!
-class DialogflowGrpc {
+class DialogflowGrpcV2Beta1 {
 
   final CallOptions _options;
 
@@ -26,17 +27,17 @@ class DialogflowGrpc {
   late SessionsClient client;
 
   // Private constructor to prevent direct initialization of the class.
-  DialogflowGrpc._(this._options) {
+  DialogflowGrpcV2Beta1._(this._options) {
     client = SessionsClient(_channel, options: _options);
   }
 
   /// Creates a DialogflowStream interface using a service account.
-  factory DialogflowGrpc.viaServiceAccount(ServiceAccount account) {
+  factory DialogflowGrpcV2Beta1.viaServiceAccount(ServiceAccount account) {
     var projectId = account.projectId;
     var uuid = Uuid().v4();
     DialogflowAuth.session = 'projects/$projectId/agent/sessions/$uuid';
 
-    return DialogflowGrpc._(account.callOptions);
+    return DialogflowGrpcV2Beta1._(account.callOptions);
   }
 
 
@@ -63,25 +64,24 @@ class DialogflowGrpc {
   /// Sends a [StreamingDetectIntentResponse] to the Dialogflow API
   /// Requires a [InputAudioConfig] and an audioStream.
   Stream<StreamingDetectIntentResponse> streamingDetectIntent(
-      InputConfigV2 config, Stream<List<int>> audioStream)   {
+      InputConfigV2beta1 config, Stream<List<int>> audioStream)   {
 
     // Create the stream, which later transmits the necessary
     // data to the Google Api.
     final request = StreamController<StreamingDetectIntentRequest>();
     // add the session to the request
 
-    print(DialogflowAuth.session);
+    //print(DialogflowAuth.session);
 
     QueryInput queryInput = QueryInput()..audioConfig = config.cast();
 
     print(queryInput);
+
     request
         .add(StreamingDetectIntentRequest()
       ..queryInput = queryInput
       ..session = DialogflowAuth.session
     );
-
-
 
     // Send the request first
     // Afterwards start streaming the audio
@@ -103,3 +103,4 @@ class DialogflowGrpc {
   }
 
 }
+
